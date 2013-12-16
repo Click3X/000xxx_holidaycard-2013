aba140a0cd761eaaf375d36405823078eb4ab748
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-12-16 17:54:00 -0500

    didnt' work

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,7 @@
 (function() {
     if (!Array.prototype.indexOf) {
+        console.log("adding Array.indexOf method");
+
         Array.prototype.indexOf = function(obj, start) {
             for (var i = (start || 0), j = this.length; i < j; i++) {
                 if (this[i] === obj) { return i; }
